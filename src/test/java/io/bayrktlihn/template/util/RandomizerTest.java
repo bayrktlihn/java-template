2c74b6a377f377cdc21dc2afd808ea5accb952b0
bayrktlihn <bayraktaralihan1995@gmail.com>
2023-08-13 01:30:23 +0300

    refactoring randomizer test

--- src/test/java/io/bayrktlihn/template/util/RandomizerTest.java
+++ src/test/java/io/bayrktlihn/template/util/RandomizerTest.java
@@ -1,15 +1,25 @@
 package io.bayrktlihn.template.util;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashSet;
+import java.util.Set;
 
 class RandomizerTest {
 
     @Test
-    void generateRandomByDateTime(){
-        String s = Randomizer.generateByDateTime();
+    void generateRandomByDateTime() {
+        // 100_000 kişi aynı anda istek yaptıgını düşün hepsi farklı olmalı
+        Set<String> items = new HashSet<>();
+        int expectedDifferenceNumbers = 100_000;
+        for (int i = 0; i < expectedDifferenceNumbers; i++) {
+            String s = Randomizer.generateByDateTime();
 
-        System.out.println(s);
+            items.add(s);
+        }
+
+        Assertions.assertEquals(expectedDifferenceNumbers, items.size());
+
     }
 }
