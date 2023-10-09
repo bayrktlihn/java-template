84b8adeb6c6c3a940cf56e62fe18e8cd705adfb2
bayrktlihn <bayraktaralihan1995@gmail.com>
2023-10-10 00:25:38 +0300

    refactoring InterestRate

--- src/main/java/io/bayrktlihn/template/util/InterestRateImpl.java
+++ src/main/java/io/bayrktlihn/template/util/InterestRateImpl.java
@@ -5,15 +5,14 @@ import lombok.ToString;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-@ToString
-public class InterestRateV2Impl implements InterestRate {
 
+@ToString
+public class InterestRateImpl implements InterestRate {
     private final BigDecimal yearly;
     private final BigDecimal monthly;
     private final BigDecimal daily;
 
-
-    public InterestRateV2Impl(BigDecimal yearly, BigDecimal monthly, BigDecimal daily) {
+    public InterestRateImpl(BigDecimal yearly, BigDecimal monthly, BigDecimal daily) {
         this.yearly = yearly;
         this.monthly = monthly;
         this.daily = daily;
@@ -22,9 +21,10 @@ public class InterestRateV2Impl implements InterestRate {
 
     public static InterestRate createWithDailyInterestRate(BigDecimal dailyInterestRate) {
         dailyInterestRate = dailyInterestRate.setScale(15, RoundingMode.HALF_UP);
-        BigDecimal yearlyInterestRate = dailyInterestRate.multiply(new BigDecimal("360"));
+        BigDecimal yearlyInterestRate = dailyInterestRate.multiply(new BigDecimal("365"));
         return createWithYearlyInterestRate(yearlyInterestRate);
     }
+
 
     public static InterestRate createWithMonthlyInterestRate(BigDecimal monthlyInterestRate) {
         monthlyInterestRate = monthlyInterestRate.setScale(15, RoundingMode.HALF_UP);
@@ -32,16 +32,14 @@ public class InterestRateV2Impl implements InterestRate {
         return createWithYearlyInterestRate(yearlyInterestRate);
     }
 
-
     public static InterestRate createWithYearlyInterestRate(BigDecimal yearlyInterestRate) {
         yearlyInterestRate = yearlyInterestRate.setScale(15, RoundingMode.HALF_UP);
         BigDecimal monthlyInterestRate = yearlyInterestRate.divide(new BigDecimal("12"), 15, RoundingMode.HALF_UP);
-        BigDecimal dailyInsterestRate = yearlyInterestRate.divide(new BigDecimal("360"), 15, RoundingMode.HALF_UP);
+        BigDecimal dailyInsterestRate = yearlyInterestRate.divide(new BigDecimal("365"), 15, RoundingMode.HALF_UP);
 
         InterestRateImpl interestRateImpl = new InterestRateImpl(yearlyInterestRate, monthlyInterestRate, dailyInsterestRate);
         return interestRateImpl;
     }
-
 
     @Override
     public BigDecimal getDaily() {
@@ -57,4 +55,19 @@ public class InterestRateV2Impl implements InterestRate {
     public BigDecimal getMonthly() {
         return monthly;
     }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof InterestRateImpl) {
+            InterestRateImpl interestRateImpl = (InterestRateImpl) obj;
+            boolean equalsDailyInterestRate = interestRateImpl.getDaily().setScale(2, RoundingMode.HALF_UP).equals(getDaily().setScale(2, RoundingMode.HALF_UP));
+            boolean equalsYearlyInterestRate = interestRateImpl.getYearly().setScale(2, RoundingMode.HALF_UP).equals(getYearly().setScale(2, RoundingMode.HALF_UP));
+            boolean equalsMonthlyInterestRate = interestRateImpl.getMonthly().setScale(2, RoundingMode.HALF_UP).equals(getMonthly().setScale(2, RoundingMode.HALF_UP));
+            return equalsDailyInterestRate && equalsYearlyInterestRate && equalsMonthlyInterestRate;
+        }
+        return false;
+    }
+
+
 }
